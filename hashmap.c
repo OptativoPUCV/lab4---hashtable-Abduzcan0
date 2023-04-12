57a40674f4c18bdb53865cdd3368917b7673397a
Replit user <>
2023-04-12 22:07:49 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -56,7 +56,8 @@ HashMap * createMap(long capacity) {
     HashMap *mapa=(HashMap*)malloc(capacity*sizeof(HashMap));
     if(mapa==NULL)exit(EXIT_FAILURE);
     
-  
+    mapa->buckets=(Pair**)malloc(capacity*sizeof(Pair*));
+    if(mapa==NULL)exit(EXIT_FAILURE);
 
     for(long i=0;i<capacity;i++){
        mapa->buckets[i]=NULL; 
