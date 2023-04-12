547b04c89cc6119b6f8f21a58e6b7183a1df3aaf
Replit user <>
2023-04-12 22:30:22 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -43,14 +43,7 @@ void insertMap(HashMap * map, char * key, void * value) {
 
     long indice=hash( key,map->capacity);
     
-    while(map->buckets[indice]!=NULL && map->buckets[indice]->key!=NULL){
-        
-        indice++;
-        if(indice==map->capacity){
-            indice=0;
-        }
-        
-    }
+    
     map->buckets[indice]=(Pair*)malloc(sizeof(Pair));
     map->buckets[indice]->key=key;
     map->buckets[indice]->value=value;
